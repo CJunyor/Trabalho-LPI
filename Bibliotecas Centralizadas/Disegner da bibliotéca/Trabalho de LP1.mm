bee05f2a0a11e95323dc0e277d85b3f0e066eed9
gau1210 <gau1210@gmail.com>
2017-06-21 00:05:54 -0300

    Add files via upload

---
+++ Bibliotecas Centralizadas/Disegner da bibliotéca/Trabalho de LP1.mm
@@ -0,0 +1,29 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1496529850879" ID="ID_1883310861" MODIFIED="1496530299288" TEXT="Trabalho de LP1">
+<node CREATED="1496529970591" ID="ID_1649450566" MODIFIED="1496530750369" POSITION="right" TEXT="Discutir o projeto">
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1496530040658" HGAP="-12" ID="ID_719473287" MODIFIED="1496530716680" POSITION="left" TEXT="Ler o problema " VSHIFT="-84">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1496530089414" ID="ID_725939876" MODIFIED="1496530153149" POSITION="right" TEXT="Escrever o c&#xf3;digo">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1496530270202" HGAP="34" ID="ID_1037892889" MODIFIED="1496530291451" TEXT="Discutir o a parte gr&#xe1;fica" VSHIFT="14">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1496530302497" HGAP="18" ID="ID_798200417" MODIFIED="1496530654402" TEXT="Projetar o designer" VSHIFT="-13">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1496530549399" HGAP="35" ID="ID_492521947" MODIFIED="1496530629754" TEXT="Montar o programa">
+<icon BUILTIN="flag-green"/>
+<node CREATED="1496530585585" HGAP="30" ID="ID_53425128" MODIFIED="1496530609607" TEXT="Testar" VSHIFT="2">
+<icon BUILTIN="ksmiletris"/>
+</node>
+</node>
+<node CREATED="1496530434022" HGAP="18" ID="ID_767612740" MODIFIED="1496530775052" TEXT="Fazer o designer" VSHIFT="13">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+</map>
